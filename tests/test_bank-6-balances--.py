09c815a347b43b859d77a2199e3a9f5b2d9f6bc4
Christian Martiny <christian.martiny@outlook.com>
2024-05-17 11:08:01 +0200

    test

--- tests/test_bank-6-balances--.py
+++ tests/test_bank-6-balances--.py
@@ -13,6 +13,7 @@ from source.models import Accounts, Account, Transactions, Transaction
 @pytest.mark.parametrize(['account_id_withdraw', 'amount', 'Accounts_change_balance', 'error'],
                          [
                              ('5', '10000', True, 0),
+                             ('5', '10000', True, 0),
                          ])
 def test_get_balance(account_id_withdraw, amount, Accounts_change_balance, error):
     conn, session = bank.connection()
